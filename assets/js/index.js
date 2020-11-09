2399a10b888066879738397ae09f9c2fa1df1877
寺岡 健太郎 <k-teraoka@PC342.local>
2020-11-09 16:28:01 +0900

    レビュー修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,10 +16,9 @@ function movePage(page){
     let slideImg = document.getElementById("slideImg");
     if(page === "prev" && currentPage > 0){
         currentPage --; //prevボタンを押してて現在のページが最初以外の時、ページ数を-1
-        pageNum.innerHTML = currentPage+1 + "/" + imgs.length; //表示したページ数を更新
     }else if (page === "next" && currentPage < imgs.length - 1){
         currentPage ++; //nextボタンを押してて現在のページが最後以外の時、ページ数を+1
-        pageNum.innerHTML = currentPage+1 + "/" + imgs.length; //表示したページ数を更新
     }
+    pageNum.innerHTML = (currentPage + 1) + "/" + imgs.length; //表示したページ数を更新
     slideImg.setAttribute("src", "assets/img/" + imgs[currentPage]); //src内の画像ファイル名を変える
 };
