126e75f4751c0a2003e66fbe45e01caba2034435
寺岡 健太郎 <k-teraoka@PC342.local>
2020-11-09 16:10:10 +0900

    スライドショー機能実装

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+let currentPage = 0;
+let imgs = [
+    "image1.jpg",
+    "image2.jpg", 
+    "image3.jpg", 
+    "image4.jpg", 
+    "image5.jpg", 
+];
+
+window.onload = function(){
+    pageNum.innerHTML = currentPage+1 + "/" + imgs.length; //ロード時、ページ数を表示
+};
+
+function movePage(page){
+    let pageNum = document.getElementById("pageNum");
+    let slideImg = document.getElementById("slideImg");
+    if(page === "prev" && currentPage > 0){
+        currentPage --; //prevボタンを押してて現在のページが最初以外の時、ページ数を-1
+        pageNum.innerHTML = currentPage+1 + "/" + imgs.length; //表示したページ数を更新
+    }else if (page === "next" && currentPage < imgs.length - 1){
+        currentPage ++; //nextボタンを押してて現在のページが最後以外の時、ページ数を+1
+        pageNum.innerHTML = currentPage+1 + "/" + imgs.length; //表示したページ数を更新
+    }
+    slideImg.setAttribute("src", "assets/img/" + imgs[currentPage]); //src内の画像ファイル名を変える
+};
